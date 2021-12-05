033758be54d8d5ad883ac54caafc1cc12fdf2594
WS269879 <WS269879@weston.ac.uk>
2021-12-05 04:05:19 +0000

    Added a silly clock

--- dd5e-client/src/app/app.component.ts
+++ dd5e-client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core'
 import {IUserDetails, LoginService} from './service/login.service'
 import {Router} from '@angular/router'
 import {CardsService} from './service/cards.service'
+import {interval} from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,22 @@ import {CardsService} from './service/cards.service'
 export class AppComponent implements OnInit{
 
   public user?: IUserDetails
+  public time?: string
 
   constructor(private _loginService: LoginService,
               private _router: Router,
               public cardsService: CardsService) {
-  }
-
-  ngOnInit(): void {
     this._loginService.getUserDetails().subscribe((user) => {
       if (user) this.user = user
       else this.user = undefined
     })
+    interval(1000).subscribe((t) => {
+      const date = new Date()
+      this.time = `${date.getHours() < 10 ? '0' + date.getHours() : date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}:${date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()}`
+    })
+  }
+
+  ngOnInit(): void {
   }
 
   public async logoutClick() {
